6db2847bd1841e7c8e2d89a7be01fcdc39286456
bfichera <bfichera@mit.edu>
2021-09-05 03:39:42 -0400

    Implement instrument templating

--- instruments/instrument.py
+++ instruments/instrument.py
@@ -1,16 +1,29 @@
 import logging
-
-from pyvisa.errors import VisaIOError
+import types
+import inspect
+import time
 
 _logger = logging.getLogger(__name__)
 
 
-class Instrument:
+class _Copy:
+    pass
 
-    def __init__(self, resource, **kws):
-        self._resource = resource
-        for k,v in kws.items():
-            self._resource.__setattr__(k, v)
 
-    def configure(self, **kws):
-        
+def _modify_resource(resource, template):
+
+    for methodname, method in inspect.getmembers(template):
+        if not methodname.startswith('__'):
+            copy = _Copy()
+            try: 
+                def func(self, *args, **kwargs):
+                    return method(self, copy, *args, **kwargs)
+                setattr(copy, methodname, getattr(resource, methodname))
+                setattr(resource, methodname, types.MethodType(func, resource))
+            except:
+                pass
+
+def open_instrument(resource_name, resource_manager, template, *args, **kwargs):
+    resource = resource_manager.open_resource(resource_name, *args, **kwargs)
+    _modify_resource(resource, template)
+    return resource
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-import logging
 import pyvisa
-
+from instruments.instrument import open_instrument
 from instruments.bk_1686B import bk_1686B
 
-# logging.basicConfig(level=logging.WARNING)
-
-#hi = Resource()
 rm = pyvisa.ResourceManager()
-me = bk_1686B('ASRL/dev/ttyUSB0::INSTR', rm, write_termination='\r', read_termination='\r')
-print(me.query('GETS'))
+bk = open_instrument(
+    'ASRL/dev/ttyUSB0::INSTR',
+    rm,
+    bk_1686B,
+    write_termination='\r',
+    read_termination='\r',
+)
+print(bk.query('GETS'))
